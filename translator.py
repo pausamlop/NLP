12bab2c57e7358b824407e12aceb52ea12c4a862
Máster IA Máster IA <masteria@tsc034.tsc.uc3m.es>
2025-01-03 20:07:57 +0100

    traductor con mayor max_length

--- translator.py
+++ translator.py
@@ -20,7 +20,11 @@ def translate_forward(translator, input):
 
     # detección del lenguaje de entrada
     input_lang = detect(input)
-    
+
+    # Si el idioma detectado es catalán (ca), lo tratamos como español (es)
+    if input_lang == "ca":
+        input_lang = "es_XX"
+
     lang_list = ["ar_AR", "cs_CZ", "de_DE", "en_XX", "es_XX", "et_EE", "fi_FI", "fr_XX", "gu_IN", "hi_IN", "it_IT", "ja_XX", "kk_KZ", 
                 "ko_KR", "lt_LT", "lv_LV", "my_MM", "ne_NP", "nl_XX", "ro_RO", "ru_RU", "si_LK", "tr_TR", "vi_VN", "zh_CN", "af_ZA", 
                 "az_AZ", "bn_IN", "fa_IR", "he_IL", "hr_HR", "id_ID", "ka_GE", "km_KH", "mk_MK", "ml_IN", "mn_MN", "mr_IN", "pl_PL", 
@@ -34,7 +38,7 @@ def translate_forward(translator, input):
         return input_lang, input
     
     # traducción   
-    output = translator(input, src_lang=input_lang, tgt_lang="es_XX")[0]["translation_text"]
+    output = translator(input, src_lang=input_lang, tgt_lang="es_XX",max_length = 1000)[0]["translation_text"]
 
     return input_lang, output
 
@@ -49,6 +53,6 @@ def translate_backwards(translator, input, language):
         return input
     
     # traducción   
-    output = translator(input, src_lang="es_XX", tgt_lang=language)[0]["translation_text"]
+    output = translator(input, src_lang="es_XX", tgt_lang=language,max_length = 1000)[0]["translation_text"]
 
     return output
